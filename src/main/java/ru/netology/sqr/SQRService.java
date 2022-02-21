925f896900ba4ed84b01bc843b56c0136be2c969
Анастасия Антонова <96870104+AntonovaAnastasiya@users.noreply.github.com>
2022-02-21 18:42:25 +0900

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,11 +2,16 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int sqrtCeil(int lower_limit, int upper_limit) {
+        
+        int sqrtCeil = 100;
+        int counter = 0;
+        
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= lower_limit && i * i <= upper_limit) {
-                return i * i;
-            }
+            if (i * i >= lowerLimit && i * i <= upperLimit) {
+                sqrtCeil = i * i;
+                counter++;
+                }
         }
-        return 0;
+        return counter;
     }
 }
